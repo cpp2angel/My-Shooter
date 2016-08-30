ef3612e697b562265b0a7de9710425d6a4566e68
cpp2angel <cpp2angel@gmail.com>
2016-08-31 07:28:16 +0900

    add new files

---
+++ Assets/Scripts/BackScroller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackScroller : MonoBehaviour {
+    public float scroll_speed = 1.2f;
+    Material myMaterial;
+
+	// Use this for initialization
+	void Start () {
+        myMaterial = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float newOffsetY = myMaterial.mainTextureOffset.y + scroll_speed * Time.deltaTime;
+        Vector2 newOffset = new Vector2(0, newOffsetY);
+        myMaterial.mainTextureOffset = newOffset;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 0.4f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void MoveControl()
+    {
+        float moveX = moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        float moveY = moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+        transform.Translate(moveX, moveY, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        MoveControl();
+	}
+}
