489e7fd2f5543e6849e2d7877d08d674944a77cf
cpp2angel <cpp2angel@gmail.com>
2016-09-08 14:48:43 +0900

    explosion 추가

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-    public float moveSpeed = 0.4f;
+    public float moveSpeed = 0.5f;
 
 	// Use this for initialization
 	void Start () {
